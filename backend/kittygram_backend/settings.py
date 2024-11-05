4ee349312a51fa9801ff5240cfe008556577c649
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-11-05 06:02:50 +0000

    Added static for backend.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -90,7 +90,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/kittygram/media/'
